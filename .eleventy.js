e5e9ff33f90d4d9b06c1394030abdd1cc16d34fd
Zuzana Jarošová <zuzana.jarosova93@seznam.cz>
2024-05-06 18:24:15 +0200

    eleventy hotovo

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,21 @@
 module.exports = function(eleventyConfig) {
-    // Výchozí výstupní složka je: _site
-    // Zkopírovat images/ do _site/images
-    eleventyConfig.addPassthroughCopy("images");
-    // Zkopírovat css/ to _site/css/
-    eleventyConfig.addPassthroughCopy("css");
-    };
 
-    module.exports = function(eleventyConfig) {
-        // ...necháme, co už v konfiguraci je, a přidáme:
-        return {
-        // možné formáty šablon
-        templateFormats: ["njk", "html", "md"],
-        // jako šablonovací jazyk zvolíme Nunjucks`
-        markdownTemplateEngine: "njk",
-        htmlTemplateEngine: "njk",
-        dataTemplateEngine: "njk",
-        }
-        };
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("images");
+
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy("css");
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
